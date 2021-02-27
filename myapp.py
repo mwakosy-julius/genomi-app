55ade8da448dd01567af3370170556fada4e529d
mwakosy-julius <64178653+mwakosy-julius@users.noreply.github.com>
2021-02-27 15:13:55 +0300

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,12 +5,15 @@ from PIL import Image
 
 st.title('WELCOME TO MY PETITE GENOMI APP')
 
-#image = Image.open('Untitled.jpg')
+#image = Image.open('chroma.jpg')
 #st.image(image, use_column_width=True)
 
-st.header('Enter Sequence In CAPS')
+st.header('Enter Sequence')
 
 sequence = st.text_area("", height=25)
+sequence = sequence.upper()
+sequence = sequence.splitlines()
+sequence = ''.join(sequence)
 
 DNA = st.button('DNA')
 RNA = st.button('RNA')
@@ -121,6 +124,22 @@ def central():
         amino_sequence+=codon
     return amino_sequence
 
+aminoAcids = central()
+
+def gc():
+    gc = sequence.count('G') + sequence.count('C')
+    total = len(sequence)
+    content = ''
+    try:
+        content = str(int(gc/total*100))+'%'
+    except ZeroDivisionError:
+        pass
+    return content
+
+def total_count():
+    duuh = str(len(sequence))
+    return duuh
+
 def percentaged():
     d = dict([
     ('A ', sequence.count('A')),
@@ -129,6 +148,127 @@ def percentaged():
     ('T ', sequence.count('T'))
     ])
     return d
+
+def percentager():
+    r = dict([
+    ('A ', sequence.count('A')),
+    ('G ', sequence.count('G')),
+    ('C ', sequence.count('C')),
+    ('T ', sequence.count('U'))
+    ])
+    return r
+
+def aminod(seq):
+    acid = dict([
+    ('Phe ', seq.count('Phe')),
+    ('Leu ', seq.count('Leu')),
+    ('Ile ', seq.count('Ile')),
+    ('Met ', seq.count('Met')),
+    ('Val ', seq.count('Val')),
+    ('Ser ', seq.count('Ser')),
+    ('Pro ', seq.count('Pro')),
+    ('Thr ', seq.count('Thr')),
+    ('Ala ', seq.count('Ala')),
+    ('Tyr ', seq.count('Tyr')),
+    ('His ', seq.count('HIs')),
+    ('Gln ', seq.count('Gln')),
+    ('Asn ', seq.count('Asn')),
+    ('Lys ', seq.count('Lys')),
+    ('Asp ', seq.count('Asp')),
+    ('Glu ', seq.count('Glu')),
+    ('Cys ', seq.count('Cys')),
+    ('Trp ', seq.count('Trp')),
+    ('Arg ', seq.count('Arg')),
+    ('Gly ', seq.count('Gly'))
+    ])
+    return acid
+number = aminod(aminoAcids)
+
+def graphd():
+    X = percentaged()
+    X_label = list(X)
+    X_values = list(X.values())
+
+    df = pd.DataFrame.from_dict(X, orient='index')
+    df = df.rename({0: 'count'}, axis='columns')
+    df.reset_index(inplace=True)
+    df = df.rename(columns = {'index': 'nucleotide'})
+    return df
+
+def graphr():
+    X = percentager()
+    X_label = list(X)
+    X_values = list(X.values())
+
+    df = pd.DataFrame.from_dict(X, orient='index')
+    df = df.rename({0: 'count'}, axis='columns')
+    df.reset_index(inplace=True)
+    df = df.rename(columns = {'index': 'nucleotide'})
+    return df
+
+def chartd():
+    X = percentaged()
+    X_label = list(X)
+    X_values = list(X.values())
+
+    df = pd.DataFrame.from_dict(X, orient='index')
+    df = df.rename({0: 'count'}, axis='columns')
+    df.reset_index(inplace=True)
+    df = df.rename(columns = {'index': 'nucleotide'})
+    
+
+    p = alt.Chart(df).mark_bar().encode(
+        x = 'nucleotide',
+        y = 'count'
+    )
+
+    p = p.properties(
+        width = alt.Step(80)
+    )
+    return p
+
+def chartr():
+    X = percentager()
+    X_label = list(X)
+    X_values = list(X.values())
+
+    df = pd.DataFrame.from_dict(X, orient='index')
+    df = df.rename({0: 'count'}, axis='columns')
+    df.reset_index(inplace=True)
+    df = df.rename(columns = {'index': 'nucleotide'})
+    
+
+    p = alt.Chart(df).mark_bar().encode(
+        x = 'nucleotide',
+        y = 'count'
+    )
+
+    p = p.properties(
+        width = alt.Step(80)
+    )
+    return p
+
+def charta(num):
+    X = num
+    X_label = list(X)
+    X_values = list(X.values())
+
+    df = pd.DataFrame.from_dict(X, orient='index')
+    df = df.rename({0: 'count'}, axis='columns')
+    df.reset_index(inplace=True)
+    df = df.rename(columns = {'index': 'amino-acid'})
+    
+
+    p = alt.Chart(df).mark_bar().encode(
+        x = 'amino-acid',
+        y = 'count'
+    )
+
+    p = p.properties(
+        width = alt.Step(20)
+    )
+    return p
+chacha = charta(number)
 
 def percentager():
     r = dict([
@@ -201,6 +341,10 @@ def translation():
         amino_sequence+=codon
     return amino_sequence
 
+aminoAcid = translation()
+numbers = aminod(aminoAcid)
+chachas = charta(numbers)
+
 def rna_rna():
     replica = ''
     for nucleotide in sequence:
@@ -229,60 +373,86 @@ def rna_dna():
         reverse += nucleotide
     return reverse
 
-
-
 replication = dna_dna()
 transcription = transcription()
-aminoAcids = central()
-percentages = percentaged()
+gc_content = gc()
+total_count = total_count()
+percentages1 = graphd()
+percentages2 = chartd()
+amino_chart = charta(number)
 
 rnaReplication = rna_rna()
 reverse_transcription = rna_dna()
 translation = translation()
-percentager = percentager()
+percentagesa = graphr()
+percentagesb = chartr()
 
 def valid():
     chance = 0
     for nucleotide in sequence:
         if nucleotide == 'A' or nucleotide == 'T' or nucleotide == 'G' or nucleotide == 'C':
             chance+=1
+        elif nucleotide == ' ':
+            st.error('Spaces are not allowed')
+            break
         else:
+            st.error('Invalid input in nucleotide number '+ str(sequence.index(nucleotide)+1))
+            break
             chance-=1
+    
+    if len(sequence) == 0:
+        st.warning("Hey don't just look there, Enter something")
+
     if chance == len(sequence) and chance > 0:
         st.success('Your DNA is fantastic')
-        st.write('DNA sequence: ',  sequence)
-        st.write('DNA replicon: ', replication)
-        st.write('DNA transript: ', transcription)
-        st.write('Amino acids: ', aminoAcids)
-        st.write('Nucleotide Count: ', percentages)
+        st.write('dna sequence   \n',  sequence)
+        st.write('dna replicon   \n', replication)
+        st.write('dna transcript   \n ', transcription)
+        st.write('AMINO ACIDS   \n', aminoAcids)
+        st.write(percentages1)
+        st.write(percentages2)
+        st.write('The G-C content in the sequence is ', gc_content)
+        st.write('The total number of nucleotides is ', total_count)
+        st.write(chacha)
 
-    if chance < len(sequence):
-        st.error('Ooops, What kind of DNA is this!')
-    if chance == 0:
-        st.warning("Hey don't just look there, Enter something")
+    
         
 def valir():
     chance = 0
     for nucleotide in sequence:
         if nucleotide == 'A' or nucleotide == 'U' or nucleotide == 'G' or nucleotide == 'C':
             chance+=1
+        elif nucleotide == ' ':
+            st.error('Spaces are not allowed')
+            break
         else:
+            st.error('Invalid input in nucleotide number '+ str(sequence.index(nucleotide)+1))
+            break
             chance-=1
-    if chance == len(sequence) and chance > 0:
-        st.success('Your RNA is fantastic')
-        st.write('RNA sequence: ',  sequence)
-        st.write('RNA replicon: ', rnaReplication)
-        st.write('DNA transript: ', reverse_transcription)
-        st.write('Amino acids: ', translation)
-        st.write('Nucleotide Count: ', percentager)
-        
-    if chance < len(sequence):
-        st.error('Ooops, What kind of RNA is this!')
-    if chance == 0:
+    
+    if len(sequence) == 0:
         st.warning("Hey don't just look there, Enter something")
 
+    if chance == len(sequence) and chance > 0:
+        st.success('Your RNA is fantastic')
+        st.write('rna sequence   \n',  sequence)
+        st.write('rna replicon   \n', rnaReplication)
+        st.write('reverse transript   \n', reverse_transcription)
+        st.write('Amino acids   \n', translation)
+        st.write(percentagesa)
+        st.write(percentagesb)
+        st.write('The G-C content in the sequence is ', gc_content)
+        st.write('The total number of nucleotides is ', total_count)
+        st.write(chachas)
+        
 if DNA:
     valid() 
 if RNA:
     valir()
+
+
+
+
+
+
 
